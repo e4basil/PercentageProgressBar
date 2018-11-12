34411f9d938e066cf17efeff09a0f08f55e0583d
basilk <basilk@zhservices.com>
2018-11-12 11:53:50 +0530

    updated README.md

--- app/src/test/java/com/android/basi/progressapp/ExampleUnitTest.kt
+++ app/src/test/java/com/android/basi/progressapp/ExampleUnitTest.kt
@@ -1,9 +1,5 @@
 package com.android.basi.progressapp
 
-import org.junit.Test
-
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
--- percentageprogressbar/src/test/java/com/android/basi/percentageprogressbar/ExampleUnitTest.java
+++ percentageprogressbar/src/test/java/com/android/basi/percentageprogressbar/ExampleUnitTest.java
@@ -1,9 +1,5 @@
 package com.android.basi.percentageprogressbar;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
